258296170bf8610fbd6cd1e6fbb55c628f0cd9e0
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2023-12-08 11:50:53 +0530

    Building Pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,30 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import LogoutButton from "../components/Header/LogoutButton";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import PostCard from "./PostCard";
+import PostForm from "../components/post-form/PostForm";
+import Container from "../components/container/Container"
 
 export{
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutButton,
+    RTE,
+    Signup,
+    Login,
+    Input,
+    AuthLayout,
+    Button,
+    PostCard,
+    PostForm,
+    Container,
 }
